a29e05a157186a9f7cb1c20de95fd1a7da6d6c44
Renan <renan.061@gmail.com>
2023-10-22 02:28:21 -0300

    basic logic (lots of TODOs)

--- arena-node/dapp.py
+++ arena-node/dapp.py
@@ -10,33 +10,126 @@ logger = logging.getLogger(__name__)
 rollup_server = environ["ROLLUP_HTTP_SERVER_URL"]
 logger.info(f"HTTP rollup_server url is {rollup_server}")
 
-def character_from_dictionary(i, d):
-    d = d[i]
-    return Character(i, d["name"], d["weapon"], d["hp"], d["atk"], d["def"], d["spd"])
 
-def decode(payload):
-    s = bytes.fromhex(payload[2:]).decode("utf-8")
-    d = json.loads(s)["characters"]
-    c0 = character_from_dictionary(0, d)
-    c1 = character_from_dictionary(1, d)
-    return c0, c1
+def hex_to_string(hex):
+    return bytes.fromhex(hex[2:]).decode("utf-8")
 
+# def character_from_dictionary(i, d):
+#     d = d[i]
+#     return Character(i, d["name"], d["weapon"], d["hp"], d["atk"], d["def"], d["spd"])
+# 
+# def decode(payload):
+#     s = bytes.fromhex(payload[2:]).decode("utf-8")
+#     d = json.loads(s)["characters"]
+#     c0 = character_from_dictionary(0, d)
+#     c1 = character_from_dictionary(1, d)
+#     return c0, c1
+ 
 def encode(d):
     return json.dumps(d).encode("utf-8").hex()
+
+def send_notice(payload):
+    notice = {"payload": encode(payload)}
+    response = requests.post(rollup_server + "/notice", json=notice)
+    logger.info(f"Received notice status {response.status_code} body {response.content}")
+
+database = {}
+
+class Battle:
+    def __init__(self, champion_hash, champion_address):
+        self.champion_hash = champion_hash 
+        self.champion_address = champion_address
+        self.champion_character = None
+        self.challenger_address = None
+        self.challenger_character = None
+        self.finished = False
+
+    def is_open(self):
+        return self.challenger_character is None
+
+    def can_begin(self):
+        return self.is_open() and self.champion_character is None
+
+    def valid_hash(self, sent_hash):
+        return self.champion_hash == sent_hash
+
+    def begin(self):
+        self.winner = None # TODO
+
+def get_champion_battle(champion_address, battle_id):
+    if champion_address not in database:
+        raise ValueError("battle does not exist -- invalid champion address")
+    champion_battles = database[champion_address]
+    if battle_id >= len(champion_battles):
+        raise ValueError("battle does not exist -- invalid battle ID")
+    return champion_battles[battle_id]
+
+def collect_reward(player_address, payload):
+    battle = get_champion_battle(payload["champion_address"], payload["battle_id"])
+    if battle.winner != player_address:
+        raise ValueError("only the winner can collect the reward")
+    if battle.finished:
+        raise ValueError("the reward has already been collected")
+    "can withdraw => emmit voucher" # TODO
+    "send notice with the voucher(?)" # TODO
+    battle.finished = True
 
 def handle_advance(data):
     logger.info(f"Received advance request data {data}")
 
-    c0, c1 = decode(data["payload"])
-    logger.info(f"These are the characters: {c0} and {c1}")
+    player_address = data["metadata"]["msg_sender"]
+    payload = json.loads(hex_to_string(data["payload"]))
 
-    result, _ = battle(c0, c1)
-    logger.info(f"The winner is: {result['winner']}")
-
-    notice = {"payload": encode(result)}
-    response = requests.post(rollup_server + "/notice", json=notice)
-    logger.info(f"Received notice status {response.status_code} body {response.content}")
-
+    match payload["method"]:
+        case "create_battle":
+            # TODO: stake stuff
+            character_hash = payload["character_hash"]
+            battle = Battle(character_hash, player_address)
+            if player_address in database:
+                database[player_address].append(battle)
+            else:
+                database[player_address] = [battle]
+            send_notice({"id": len(database[player_address]) - 1})
+        case "join_battle":
+            champion_address = payload["champion_address"]
+            if champion_address in database:
+                champion_battles = database[champion_address]
+                battle_id = payload["battle_id"]
+                if battle_id < len(champion_battles["battles"]):
+                    battle = champion_battles[battle_id]
+                    if battle.is_open():
+                        # TODO: stake stuff
+                        battle.challenger_address = payload[player_address]
+                        battle.challenger_character = payload["character"]
+                    else:
+                        "TODO" # TODO: battle already has two players
+                else:
+                   "TODO" # TODO: battle does not exist -- invalid battle id
+            else:
+               "TODO" # TODO: battle does not exist -- invalid champion address
+        case "begin_battle":
+            champion_address = player_address
+            if champion_address in database:
+                champion_battles = database[champion_address]
+                battle_id = payload["battle_id"]
+                if battle_id < len(champion_battles["battles"]):
+                    battle = champion_battles[battle_id]
+                    if battle.can_begin():
+                        if battle.valid_hash("hash(payload['character'])"):
+                            battle.champion_character = payload["character"]
+                            battle.begin()
+                        else:
+                            battle.winner = battle.challenger_address
+                    else:
+                        "TODO" # TODO: battle already begun
+                else:
+                   "TODO" # TODO: battle does not exist -- invalid battle id
+            else:
+               "TODO" # TODO: battle does not exist -- invalid champion address
+        case "collect_reward":
+            collect_reward(player_address, payload)
+            # TODO: do the same with other cases
+    
     return "accept"
 
 def handle_inspect(data):
